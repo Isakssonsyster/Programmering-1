62a39a32c501b26d96d95a4b67ab94e476abbcfe
Isakssonsyster <isabella.isaksson@elev.ga.ntig.se>
2019-10-26 15:10:14 +0200

    first commit double radius

---
+++ Hemläxa vecka 43/src/Radie.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Radie {
+
+	public static void main(String[] args) {
+		
+		Math doubleRadius;
+		doubleVolume(doubleRadius);
+	}
+
+	private static void doubleVolume(Object radius) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(system.in);
+		System.out.println("Skriv in cirkelns radie: ");
+		int radie = input.nextInt();
+		
+	}
+}
