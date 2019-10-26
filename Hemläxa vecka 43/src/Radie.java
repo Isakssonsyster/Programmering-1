63712d573c238a9950ccb4a9a7a7472284e3c591
Isakssonsyster <isabella.isaksson@elev.ga.ntig.se>
2019-10-26 15:37:52 +0200

    commit 2 radie

--- Hemläxa vecka 43/src/Radie.java
+++ Hemläxa vecka 43/src/Radie.java
@@ -3,15 +3,17 @@ public class Radie {
 
 	public static void main(String[] args) {
 		
-		Math doubleRadius;
+		Math doubleRadius = null;
 		doubleVolume(doubleRadius);
 	}
 
-	private static void doubleVolume(Object radius) {
+	public static void doubleVolume(Object radius) {
 		// TODO Auto-generated method stub
-		Scanner input = new Scanner(system.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("Skriv in cirkelns radie: ");
 		int radie = input.nextInt();
-		
+		Math pi;
+		Math volym = 4*pi*radie^3/3;
+		System.out.println("Cirkelns volym: " + volym);
 	}
 }
