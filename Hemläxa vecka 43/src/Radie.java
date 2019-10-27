fe5912382f83d7d048f1524ff58a201cd0fcba9a
Isakssonsyster <isabella.isaksson@elev.ga.ntig.se>
2019-10-27 21:07:37 +0100

    Last commit läxa vecka 43

--- Hemläxa vecka 43/src/Radie.java
+++ Hemläxa vecka 43/src/Radie.java
@@ -1,3 +1,5 @@
+// Uppgift 1
+
 import java.util.Scanner;
 public class Radie {
 
@@ -34,9 +36,9 @@ public class Radie {
 	
 	
 // Uppgift 2
+	
 private static void stringName(int name) {
 	// TODO Auto-generated method stub
-	int Name = 0;
 	Scanner read = new Scanner(System.in);
 	
 	//Skapar en ny scanner vid namn read
@@ -54,6 +56,7 @@ private static void stringName(int name) {
 
 
 // Uppgift 3
+
 private static void stringCount(String str, char c) {
 	// TODO Auto-generated method stub
 	// Gör en method till uppgift 3 som visar att den ska räkna ut antalet gånger C återkommer i en string
@@ -72,5 +75,52 @@ private static void stringCount(String str, char c) {
 	// Räknar ut hur många gånger C skrivs i Stringen man skriver in
 	
 	return reopeatsOfC; //Ska visa hur många gånger som C används
+} 
 }
+
+
+
+// Uppgift 4
+
+public static String sjorovarSpråket(String str) {
+	String Result = " ";
+	
+	Scanner input = new Scanner(System.in);
+	int aWord = input.nextInt();
+	// Skapar en scanner för input och en variabel för det man ska skriva in
+	
+	for(int i = 0; i < str.length(); i++) {
+		Object theResult;
+		if (str.charAt(i) == ' ') {
+			
+			theResult = theResult + ' ';
+			
+		} else if (str.charAt(i) == 'x') {
+			
+			theResult = theResult + "koksos";
+			
+		} else if (str.charAt(i) == 'c')
+		
+			theResult = theResult + "kok";
+		
+		} int i;
+		 if (str.charAt(i) == 'a' || str.charAt(i) == 'o' || str.charAt(i) == 'u' || str.charAt(i) == 'å'
+				 
+				|| str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'y' || str.charAt(i) == 'ä'
+				
+				|| str.charAt(i) == 'ö' || str.charAt(i) == 'A' || str.charAt(i) == 'O' || str.charAt(i) == 'U'
+				
+				|| str.charAt(i) == 'Å' || str.charAt(i) == 'E' || str.charAt(i) == 'I' || str.charAt(i) == 'Y'
+				
+				|| str.charAt(i) == 'Ä' || str.charAt(i) == 'Ö') {
+			 
+			 	String theResult = theResult + str.charAt(i);
+			
+		 } else {
+			 
+			 String theResult = theResult + str.charAt(i) + "o" + str.charAt(i);
+			 
+		}
+}
+} // Jag förstår inte den sista
 
